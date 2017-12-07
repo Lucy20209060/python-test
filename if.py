4ca28721e854dd61dc93b419afc9740c9a8ddd71
Lucy20209060 <550502661@qq.com>
2017-12-07 23:23:21 +0800

    list

---
+++ if.py
@@ -0,0 +1,4 @@
+if 1+1 == 2:
+	print "true"
+else:
+	print "false"
---
+++ str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 头部注释 防止编码错误 导致报错
+
+str = 'hello world!!'
+# 字符串截取
+# 1-7 截取1-7位 不包括第七位
+tem = str[1:7]
+print(tem)
+
+# 截取到第七位 不包括第七位
+tem2 = str[:7]
+print(tem2)
+
+# 倒数第3位
+print(str[-3])
