e963b3bff0c9aaec3eeb2b14b81f7578e32ea789
Lucy20209060 <550502661@qq.com>
2019-01-15 16:50:47 +0800

    feat:Class

--- day12-20/class.py
+++ day12-20/class.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # 类
 class Dog:
 	def __init__(self,name,age):
 		self.name = name
 		self.age = age
 	def sayName(self):
-		print('my name is '+self.name)
+		print('my name is ' + self.name + ', I am ' + str(self.age) + ' years old')
 
 
 mydog = Dog('lucy',20)
 mydog.sayName()
+print(mydog.name)
