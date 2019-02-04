1147948674ad9a8b87ffaa48d60ef7dd6e1ff442
Lucy20209060 <550502661@qq.com>
2019-02-04 22:25:06 +0800

    迭代器

--- day02-03/list.py
+++ day02-03/list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import os
+from collections import Iterator  #迭代器
+from collections import Iterable
 
 # print [x*x for x in range(1, 11) if x > 2]
 
@@ -13,14 +15,27 @@ import os
 # for x in g:
 #     print x
 
-def foo():
-    print 'step 1'
-    yield 1
-    print 'step 2'
-    yield 2
-    print 'step 3'
-    yield 3
+# def foo():
+#     print 'step 1'
+#     yield 1
+#     print 'step 2'
+#     yield 2
+#     print 'step 3'
+#     yield 3
 
-f = foo()
-print f.next()
-print f.next()
+# f = foo()
+# print f.next()
+# print f.next()
+
+# g = iter([1,2,3])
+# print g.next()
+# print g.next()
+# print g.next()
+# print g.next()
+
+g = (x for x in range(1,11))
+# print g.next()
+l = [1,2,3,4,5]
+
+print isinstance(g,Iterator)     #判断是不是迭代器
+print isinstance(l,Iterable)
