88cc2adc4cdf32253fb5cd8ec6d416ab5d6f22e9
Lucy20209060 <550502661@qq.com>
2019-02-26 16:24:37 +0800

    文件操作

---
+++ OS/os_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+# a = os.walk('./')
+# for item in a:
+#     print(item)
+# os.chdir("../")
+
+# getcwd = os.getcwd()
+# getcwdu = os.getcwdu()
+# print(getcwd, type(getcwd))
+# print(getcwdu, type(getcwdu))
+
+# a = os.access()
+# print(a)
+
+path = os.path
+print(path.exists('/Users/luchao/Desktop/2019/pythonTest/OS/os_test.py'))
